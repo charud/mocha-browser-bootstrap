05c6e24d346f38226c22723224b7b7682b7bfdb3
Charlie Rudenstål <charlie4@gmail.com>
2012-07-25 18:56:06 +0300

    Update src/array.test.js

--- src/array.test.js
+++ src/array.test.js
@@ -1,10 +1,7 @@
+// Only required for testing in terminal/Node.js
 if(typeof should == "undefined") {
   var should = require('chai').should(); 
 }
-
-// For future reference, if you'd like to integrate these tests
-// (that are unfortunately browser dependennt) into a build system
-// on a server: http://hackingon.net/post/Testing-Browser-dependent-JavaScript.aspx
 
 describe('Array', function() {
 
